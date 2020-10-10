7ce142ac69c9df2438341391e9c34d17a5fb923c
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-10 00:09:37 -0700

    create Player class

--- canvas.js
+++ canvas.js
@@ -1,4 +1,15 @@
 const canvas = document.querySelector('canvas');
 
+const canvas2d = canvas.getContext('2d');
+
 canvas.width = window.innerWidth;
 canvas.height = innerHeight;
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+}
