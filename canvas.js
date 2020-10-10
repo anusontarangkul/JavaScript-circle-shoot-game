38e797a1fb888034134446ffa2f948e2f16d1538
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-10 00:49:26 -0700

    add draw player function

--- canvas.js
+++ canvas.js
@@ -6,10 +6,22 @@ canvas.width = window.innerWidth;
 canvas.height = innerHeight;
 
 class Player {
+
     constructor(x, y, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+    draw() {
+        canvas2d.beginPath();
+        canvas2d.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        canvas2d.fillStyle = this.color;
+        canvas2d.fill()
+
+
     }
 }
+const player = new Player(100, 100, 30, 'blue')
+player.draw();
+
