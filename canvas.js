80030224373e11b667ebca5065a420387e494130
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-10 16:49:27 -0700

    add projectile class

--- canvas.js
+++ canvas.js
@@ -8,10 +8,10 @@ canvas.height = innerHeight;
 class Player {
 
     constructor(x, y, radius, color) {
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
     }
     draw() {
         canvas2d.beginPath();
@@ -22,9 +22,31 @@ class Player {
 
     }
 }
+
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+    }
+    draw() {
+        canvas2d.beginPath();
+        canvas2d.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        canvas2d.fillStyle = this.color;
+        canvas2d.fill()
+    }
+}
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, 'blue')
 player.draw();
 
+addEventListener('click', (event) => {
+    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+
+
+    projectile.draw();
+});
