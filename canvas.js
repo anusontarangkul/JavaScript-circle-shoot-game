45bc506c86cb1facdd10c5f80071985c6088c71b
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-10 19:33:13 -0700

    update remove projectiles for all sides

--- canvas.js
+++ canvas.js
@@ -111,8 +111,16 @@ function animate() {
     animationId = requestAnimationFrame(animate);
     canvas2d.clearRect(0, 0, canvas.width, canvas.height)
     player.draw();
-    projectiles.forEach((projectile) => {
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+
+        // remove projectiles once out of screen
+        if (projectile.x + projectile.radius < 0 || projectile.x - projectile.radius > canvas.width || projectile.y + projectile.radius < 0 || projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(index, 1);
+            }, 0)
+
+        }
     })
     enemies.forEach((enemy, index) => {
         enemy.update()
@@ -137,6 +145,7 @@ function animate() {
 }
 
 addEventListener('click', (event) => {
+    console.log(projectiles)
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = {
         x: Math.cos(angle),
