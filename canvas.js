8c3e8181f45f4116312b9a0741101ab2f1bcc5c8
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-10 17:08:19 -0700

    add moving projectile function

--- canvas.js
+++ canvas.js
@@ -37,6 +37,11 @@ class Projectile {
         canvas2d.fillStyle = this.color;
         canvas2d.fill()
     }
+
+    update() {
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
 }
 const x = canvas.width / 2;
 const y = canvas.height / 2;
@@ -44,9 +49,18 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, 'blue')
 player.draw();
 
-addEventListener('click', (event) => {
-    const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 })
 
 
+function animate() {
+    requestAnimationFrame(animate);
     projectile.draw();
+    projectile.update();
+}
+
+addEventListener('click', (event) => {
+
+
 });
+
+animate();
