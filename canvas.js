d4618162d699fafa6c9e11f939a8bd695056c0c4
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-10 22:06:04 -0700

    add gsap animation

--- canvas.js
+++ canvas.js
@@ -138,8 +138,10 @@ function animate() {
 
             // when projectiles touch enemy
             if (dist - enemy.radius - projectile.radius < 1) {
-                if (enemy.radius - 10 > 10) {
-                    enemy.radius -= 10;
+                if (enemy.radius - 10 > 5) {
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    });
                     setTimeout(() => {
                         projectiles.splice(projectileIndex, 1);
                     }, 0)
