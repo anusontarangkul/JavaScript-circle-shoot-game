f00e3efbb7550d9184f197f93694a8cc098b7603
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-10 19:49:32 -0700

    add coloring to eneimes and background

--- canvas.js
+++ canvas.js
@@ -71,7 +71,7 @@ class Projectile {
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 
 
 const projectile = new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 })
@@ -93,7 +93,8 @@ function spawnEnemies() {
             x = Math.random() * canvas.width;
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
         }
-        const color = 'green';
+
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
         const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
         const velocity = {
             x: Math.cos(angle),
@@ -109,7 +110,8 @@ let animationId
 
 function animate() {
     animationId = requestAnimationFrame(animate);
-    canvas2d.clearRect(0, 0, canvas.width, canvas.height)
+    canvas2d.fillStyle = 'rgba(0,0,0,0.1)'
+    canvas2d.fillRect(0, 0, canvas.width, canvas.height)
     player.draw();
     projectiles.forEach((projectile, index) => {
         projectile.update()
@@ -145,14 +147,13 @@ function animate() {
 }
 
 addEventListener('click', (event) => {
-    console.log(projectiles)
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
     }
     projectiles.push(
-        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
+        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
     )
 
 
